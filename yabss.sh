c049f9023b6ba25981153dace8095130f88e458c
slopjong <git@slopjong.de>
2012-09-12 21:06:49 +0200

    Code improvement

--- yabss.sh
+++ yabss.sh
@@ -2,14 +2,14 @@
 
 # calculate the amount of lines of the passed file
 lines(){
-	# wc -l could have been useful too
-	echo $(cat -n $1 | egrep -c --regexp="^\s*[[:digit:]]")
+    #echo $(cat -n $1 | egrep -c --regexp="^\s*[[:digit:]]")
+    echo ${$(wc -l $1)/$1}
 }
 
 # ping the neighbour hosts in your network based on arp requests
 # Note: once it runs you can't stop it until it's finished
 arpingnet(){
-	for i in {1..254}; do arping -c1 $1.$i | grep reply; done
+    for i in {1..254}; do arping -c1 $1.$i | grep reply; done
 }
 
 # [0] http://www.linuxjournal.com/content/return-values-bash-functions
